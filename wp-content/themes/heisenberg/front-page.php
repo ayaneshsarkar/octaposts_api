45f4708b83450edb31eaa28764e18ce4f27dc019
Ayanesh Sarkar <ayaneshsarkat@gmail.com>
2021-07-12 02:24:37 +0530

    Initial Commit :heavy_check_mark:

--- wp-content/themes/heisenberg/front-page.php
+++ wp-content/themes/heisenberg/front-page.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
-  <h1>Say My Name...</h1>
+  <h1 style="margin-bottom: 25px;"><?php the_title(); ?></h1>
+  <p style="white-space: pre-line;"><?php the_content(); ?></p>
 <?php get_footer(); ?>
