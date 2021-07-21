8619948363bd069e663e86422f1433f16897de4b
Ayanesh Sarkar <ayaneshsarkat@gmail.com>
2021-07-21 15:51:42 +0530

    Initial Commit :heavy_check_mark:

--- README.md
+++
@@ -1,1 +0,0 @@
-This is Octaposts API.
--- wp-content/themes/heisenberg/front-page.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-  <h1 style="margin-bottom: 25px;"><?php the_title(); ?></h1>
-  <p style="white-space: pre-line;"><?php the_content(); ?></p>
-<?php get_footer(); ?>
